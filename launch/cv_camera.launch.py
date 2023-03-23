e6f1ff6425fdd2ce40822b1f9eea6d22beb6d54c
salogranada <salogranada494@gmail.com>
2023-03-23 11:53:45 -0500

    UPDATE: launch and params with new configs

--- launch/cv_camera.launch.py
+++ launch/cv_camera.launch.py
@@ -1,10 +1,12 @@
 """Launch the vision stack in a component container."""
 import os
+import cv2
+
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import LoadComposableNodes, Node
+from launch_ros.actions import LoadComposableNodes, Node, ComposableNodeContainer
 from launch.actions import (
     DeclareLaunchArgument,
-    SetEnvironmentVariable,
+    # SetEnvironmentVariable,
     GroupAction,
 )
 from launch.conditions import IfCondition, UnlessCondition
@@ -12,13 +14,44 @@ from launch_ros.descriptions import ComposableNode
 from launch.substitutions import LaunchConfiguration
 from launch import LaunchDescription
 
-camera_names_ports = ["left", "right", "back", "zoom"]
+from nav2_common.launch import RewrittenYaml
+from python_tools.launch_utils import find_cameras, read_cams_ports
 
+# Check current running device to determine which camera ports to use
+running_device = os.getenv(key="BOARD_VERSION", default="local")
+
+# List of cameras to be used and their properties
+camera_handlers = find_cameras(
+    running_device=running_device,
+    ports_file=os.path.join(
+        get_package_share_directory("video_mapping"), "launch", "vision_ports.yaml"
+    ),
+)
 
 def generate_launch_description():
 
-    config = os.path.join(
-        get_package_share_directory("cv_camera"), "launch", "usb_camera_params.yaml"
+    use_respawn = LaunchConfiguration("use_respawn")
+
+    vision_config = os.path.join(
+        get_package_share_directory("video_mapping"), "launch", "vision_params.yaml"
+    )
+    vision_param_yaml = read_cams_ports(vision_config)
+
+    # Encode the fourcc string into VideoWriter_fourcc format
+    param_substitutions = {
+        f"{camera.label}.ros__parameters.fourcc": str(
+            float(cv2.VideoWriter_fourcc(*prop["ros__parameters"]["fourcc"]))
+        )
+        for node, prop in vision_param_yaml.items()
+        for camera in camera_handlers
+        if node == camera.label
+    }
+
+    # Use the RewrittenYaml class (from nav2_common) to do the param substitutions
+    configured_params = RewrittenYaml(
+        source_file=vision_config,
+        param_rewrites=param_substitutions,
+        convert_types=True,
     )
 
     return LaunchDescription(
@@ -28,6 +61,11 @@ def generate_launch_description():
                 "use_composition",
                 default_value="True",
                 description="Whether to use node composition",
+            ),
+            DeclareLaunchArgument(
+                "use_respawn",
+                default_value="True",
+                description="Whether to respawn if a node crashes. Applied when composition is disabled.",
             ),
             GroupAction(
                 condition=IfCondition(LaunchConfiguration("use_composition")),
@@ -42,13 +80,20 @@ def generate_launch_description():
                         target_container="vision_kronos",
                         composable_node_descriptions=[
                             ComposableNode(
-                                parameters=[config],
+                                parameters=[
+                                    configured_params,
+                                    {
+                                        "device_id": camera.device_id,
+                                        "port": camera.port,
+                                    },
+                                ],
                                 package="cv_camera",
                                 plugin="cv_camera::Driver",
-                                namespace=camera_name,
-                                name=f"cv_camera_{camera_name}",
+                                name=f"{camera.label}",
+                                extra_arguments=[{"use_intra_process_comms": True}],
                             )
-                            for camera_name in camera_names_ports
+                            for camera in camera_handlers
+                            if camera.device_id is not None
                         ],
                     ),
                 ],
@@ -58,14 +103,22 @@ def generate_launch_description():
                 condition=UnlessCondition(LaunchConfiguration("use_composition")),
                 actions=[
                     Node(
-                        parameters=[config],
+                        parameters=[
+                            configured_params,
+                            {
+                                "device_id": camera.device_id,
+                                "port": camera.port,
+                            },
+                        ],
                         package="cv_camera",
                         executable="cv_camera_node",
-                        name=f"cv_camera_{camera_name}",
+                        name=f"{camera.label}",
                         output="screen",
-                        namespace=camera_name,
+                        respawn=use_respawn,
+                        respawn_delay=2.0,
                     )
-                    for camera_name in camera_names_ports
+                    for camera in camera_handlers
+                    if camera.device_id is not None
                 ],
             ),
         ]
