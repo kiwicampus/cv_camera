c173d732b9a8fa764e4e562eaa8ccbe4830984e1
jhairssteven <71862429+jhairssteven@users.noreply.github.com>
2025-02-27 10:23:41 -0500

    [FEAT] Allow each camera to be launched with a separate intrisinc file

--- launch/kiwi.launch.py
+++ launch/kiwi.launch.py
@@ -1,47 +1,56 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
 camera_names_device_id = {
-    "left": 0,
+    "left": {
+        'device_id': 1,
+        'intrinsic_filename': 'intrinsic_calibration_params_2_1mm.yaml'
+        },
+    "econ_wide_len": {
+        'device_id': 0,
+        'intrinsic_filename': 'intrinsic_calibration_params_econ.yaml'
+        },
     # "right": 2,
     # "rear": 4,
     # "zoom": 6,
 }
+
 vision_bringup_path = get_package_share_directory("vision_bringup")
-cv_camera_path = get_package_share_directory("cv_camera")
-intrinsic_file = "file:///" + os.path.join(cv_camera_path, "launch", "camera_info.yaml")
+
 try:
     params_path = os.path.join(vision_bringup_path, "params", "vision_params.yaml")
+    params_file = 'vision_params.yaml'
 except:
-    print("Unable to load config params", flush=True)
+    print("Unable to load vision config params", flush=True)
 
 
 def generate_launch_description():
 
-    camera_info_url = LaunchConfiguration("camera_info_url")
-    declare_camera_info_url_cmd = DeclareLaunchArgument(
-        "camera_info_url",
-        default_value=intrinsic_file,
-        description="path to the camera info file",
-    )
-    params_file = LaunchConfiguration("params_file")
+    params_fileparams_file = LaunchConfiguration("params_file")
     declare_params_file_cmd = DeclareLaunchArgument(
         "params_file",
-        default_value=params_path,
-        description="path to the params file",
+        default_value="vision_params.yaml",
+        description="path to the vision params file",
     )
 
     nodes = []
-    for camera_name, device_id in camera_names_device_id.items():
+    for camera_name, items in camera_names_device_id.items():
+        device_id = items['device_id']
+        intrinsic_filename = items['intrinsic_filename']
+
         node = Node(
             parameters=[
-                params_file,
-                {"device_id": device_id},
-                {"intrinsic_file": camera_info_url},
+                PathJoinSubstitution([vision_bringup_path, "params", params_file]),
+                {
+                    "port": str(device_id),
+                    "intrinsic_file": PathJoinSubstitution(
+                        [vision_bringup_path, "configs", "calibration", intrinsic_filename]
+                    ),
+                },
             ],
             package="cv_camera",
             executable="cv_camera_node",
@@ -55,7 +64,6 @@ def generate_launch_description():
         [
             # Set env var to print messages to stdout immediately
             SetEnvironmentVariable("RCUTILS_LOGGING_BUFFERED_STREAM", "1"),
-            declare_camera_info_url_cmd,
             declare_params_file_cmd,
             *nodes,
         ]
