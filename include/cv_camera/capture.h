b7a594e769e520b6bfc809be238754c37b42fd3f
garzonwilmer5 <garzonwilmer5@gmail.com>
2026-02-16 20:33:00 +0000

    [FEAT] humble compatible

--- include/cv_camera/capture.h
+++ include/cv_camera/capture.h
@@ -7,7 +7,7 @@
 #include <string>
 
 #include <rclcpp/rclcpp.hpp>
-#include <cv_bridge/cv_bridge.hpp>
+#include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
 #include <sensor_msgs/image_encodings.hpp>
